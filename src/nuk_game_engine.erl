b0bb6325010d7f0bf09a4eea6e7ef796ff50cf8e
Unix1 <unix1@live.com>
2015-12-20 04:34:33 -0800

    more docblocks for #13

--- src/nuk_game_engine.erl
+++ src/nuk_game_engine.erl
@@ -1,5 +1,11 @@
 %%%-------------------------------------------------------------------
-%% @doc nuk game engine
+%% @doc `nuk_game_engine' module
+%%
+%% This is a behavior that all game engines must implement. It is also the only
+%% logic that game engines need implement. All callback function returns allow
+%% engines to: (1) set the arbitrary state that's relevant to the game being
+%% implemented, (2) get callbacks for important game events with the arbitrary
+%% state previously set.
 %% @end
 %%%-------------------------------------------------------------------
 
